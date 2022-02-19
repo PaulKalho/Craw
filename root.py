0deef19d10609e4bdef0cd9ffdc259f5afd9705a
PaulKalho <67231903+PaulKalho@users.noreply.github.com>
2022-02-19 20:54:21 +0100

    Added Google-Sheets functionality
    Now Craw is able to put Data into google-sheets.
    Working with google cloud service api

--- root.py
+++ root.py
@@ -1,3 +1,4 @@
+from re import M
 from time import time
 import requests
 import json
@@ -9,6 +10,42 @@ import os
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 from datetime import date, datetime, timedelta
+
+# GOOGLE AUTH 
+
+import os.path
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SERVICE_ACCOUNT_FILE = 'keys.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '177LVKVaM5a-7tokmpp8anueSLgyBN2DsYvlVA-1CKM8'
+
+
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+
+
+
+#In 2D array speichern oder gibt es andere mlöglichkeit?
+
+
+
+
+
+
+#print(request)
+
+#Ende
 
 wb = load_workbook('Daten.xlsx') 
 
@@ -33,6 +70,7 @@ elif(datum_weekday == 6):
     tradeDate = (datum - timedelta(2)).strftime('%Y%m%d')
 
 #Get trade Date from Website
+
 
 
 
@@ -103,7 +141,13 @@ print(            """\n
                             ::::  :::. .:::                     :::. .:::  :::.           
                                 """ )
 
+inc = 4
+ch = 'B'
+
 for b in range (0, 8):
+
+    inc = 4
+    
 
     if b >= 1: 
         cord_col_a = cord_col_a + 3
@@ -125,7 +169,13 @@ for b in range (0, 8):
 
     print("Progress: " + str(progress) + "%\n")
 
+    
+
     for l in range(0 , len(info_data["infoData"])):
+
+        rangeS = "Test!" + str(ch) + str(inc) #Name der Tabelle! 
+        
+        
         progress = round(progress + (12.5/len(info_data["infoData"])), 2)
 
 
@@ -174,51 +224,79 @@ for b in range (0, 8):
             isCme = True
 
             
-         
+
 
         ws[get_column_letter(cord_col_a) + str(cord_a)] = info_data["infoData"][l]["name"]
         ws[get_column_letter(cord_col_a) + str(cord_b)] = "Monat:"
         ws[get_column_letter(cord_col_b) + str(cord_b)] = "Total Volume:"
-
+        name = info_data["infoData"][l]["name"]
         i = 0 #für schleife monate reset
+
+        aoa = [[name], ["MONAT", "TOTAL"]]
+       
+        
 
         cord_b = cord_b + 1
 
-        while i < 3: #Monate Schleife
+        while i < 3:
 
             if(isCme == True):
-                if i == len(data_Cme["monthData"]):
+                if i == len(data_Cme["monthData"]): #Exit wenn nicht mehr monate # Dann im array leer!
                     cord_b = cord_b + 1
                     break
-
-                print(len(data_Cme["monthData"]))
+ 
                 ws[get_column_letter(cord_col_a) + str(cord_b)] = data_Cme["monthData"][i]["month"]
-
                 ws[get_column_letter(cord_col_b) + str(cord_b)] = data_Cme["monthData"][i]["totalVolume"]
+
+                month = data_Cme["monthData"][i]["month"]
+                totalVolume = data_Cme["monthData"][i]["totalVolume"]
+                arrayMonat = [month, totalVolume]
+                aoa.append(arrayMonat)
+
+                request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+                                range=rangeS ,valueInputOption="USER_ENTERED", body={"values": aoa}).execute()
+        
 
             if(isCme == False):
 
                 if i == len(data_ice): #Bug fix "Out of Range z.190"
                     cord_b = cord_b + 1
                     break
-
+                
                 ws[get_column_letter(cord_col_a) + str(cord_b)] = data_ice[i]["marketStrip"]
-
                 ws[get_column_letter(cord_col_b) + str(cord_b)] = data_ice[i]["volume"]
 
+                month = data_ice[i]["marketStrip"]
+                totalVolume = data_ice[i]["volume"]
+                arrayMonat = [month, totalVolume]
+                aoa.append(arrayMonat)
+
+                request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+                                range=rangeS ,valueInputOption="USER_ENTERED", body={"values": aoa}).execute()
+
             i = i + 1 #Increment
+           
+
 
             cord_b = cord_b + 1
 
+        inc = inc + 6
 
+        
         #Abstand zwischen neuen Datensätzen:
 
         cord_b = cord_b + 2
         cord_a = cord_a + 6
+
+        
     
         i = 0 #reset für nächsten durchlauf
 
+    ch = chr(ord(ch) + 3)
     wb.save("Daten.xlsx")
+
+
+
 print(""" \n 
 ███████╗██╗███╗░░██╗██╗░██████╗██╗░░██╗██╗
 ██╔════╝██║████╗░██║██║██╔════╝██║░░██║██║
