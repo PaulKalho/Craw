67d44a1c5ab68dfa03e820cb54aaefd3e698bcad
PaulKalho <kalhornpaul@gmail.com>
2022-03-07 19:03:12 +0100

    Organized

--- excel.py
+++ excel.py
@@ -4,20 +4,23 @@ from time import time
 import requests
 import json
 import datetime
+import os
 import sched, time
 import logging
+
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 from datetime import date, datetime, timedelta
-#Google Auth for Sheets API:
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
 
 ##Dependencies##
 #requests
-#openpyxl #Für excel
+#openpyxl
 #googleapiclient  pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
-
-# Call the Sheets API #
+# GOOGLE AUTH 
+import os.path
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -27,19 +30,19 @@ creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 SAMPLE_SPREADSHEET_ID = '1E5-AezYFSapijKRCwsLUfD16qsq2HTh3f3s-pgGFzNo'
-#SAMPLE_SPREADSHEET_ID = '177LVKVaM5a-7tokmpp8anueSLgyBN2DsYvlVA-1CKM8'
+
+
 
 service = build('sheets', 'v4', credentials=creds)
 
+# Call the Sheets API
 sheet = service.spreadsheets()
 
-#######################
+#Ende
 
-#### Logging #####
+#Log
 logging.basicConfig(filename="logCraw.log", level=logging.INFO, format='%(asctime)s - %(message)s')
-#################
 
-#### Set trade Date ####
 datum = (datetime.now() - timedelta(1))
 datum_weekday = datum.weekday()
 
@@ -49,7 +52,9 @@ elif(datum_weekday == 5):
     tradeDate = (datum - timedelta(1)).strftime('%Y%m%d')
 elif(datum_weekday == 6):
     tradeDate = (datum - timedelta(2)).strftime('%Y%m%d')
-#########################
+
+
+#Get trade Date from Website
 
 headers = {
     "Accept": "application/json",
@@ -59,62 +64,6 @@ headers = {
 
 logging.info("START craw.py")
 s = sched.scheduler(time.time, time.sleep)
-
-def BeautifulPrintouts(forw):
-
-    if(forw == "start"):
-        print(            """\n                                                                     
-                                        .......                                               
-                                :::::=@@@@@@@-::::.                                         
-                            .::-%@@%%%*******#%@@@#::::::::::::::::::.                      
-                        .---*%%%******************#%%%%%%%%####%%%%%%*-------               
-                        .=*%%%#******#%%%#*****************************#%%%%%%%===-           
-                    =+%#*********%#*#@%%#***********************************%%%#=-         
-                    ++%#***********@@+*#%@%****************####***##########*****#%#+-       
-                    @@*************%%@@%%%#************#####%%%########%%%%@#******#%#+:     
-                **@@*************##%%%%#*************#####%%%%%%###*###%%%%%*******%@=     
-                +#****%%%%@@@%#******###***********#####***##%%%##%%%###%%%%@@***###*####:   
-            +#*+====+++++++#%@@##*********+++*****##%######%###%%%%%%%%%%%%###*#%#*##@@-   
-            =#*+====*****#%%%@@@@@@####****+***###****##%%%%###%%%%%%%%%%%%%%*%#+#%#*#%##%%. 
-        =%*+==+*****@@@#+++++=--=@@@@%##%###%%%%####***####%%%%%%%%%%%%%%%%##%@%*###*#%@@: 
-        -@#===+*****@%-=+-.:---%@@@**@@%###*****%#%%%%%%%#%%%%%%%%#%%%%%%%##%%%%@@*#%#*#%@@: 
-        -@#=+***%@@@--:-+=-*@@@+=**@@%%%%%%%#░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗░░░*#%#*@@
-    :@%=+***%@+===++**@@@*==+****@@%%%%%%%%██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║░░░%%%##@@
-    :-%#++*#%*+::--=+%#++++*****@@%%%%%%%%%%██║░░╚═╝██████╔╝███████║░╚██╗████╗██╔╝░░░%%%%%@@
-    .@%=+##%+:=+=+%%%#++****#########%%%%#%%%██║░░██╗██╔══██╗██╔══██║░░████╔═████║░░░░%%%%%@@
-    --%#**%*=-:+*#%++++**###****.    .***#%%%%╚█████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░██╗@%%%%@@
-    @@=+@#===+###++****##**+:            =@%##░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝@%%%%@@
-    @@###+:=**#*+**####+-                :+*##%%%%%%%%%%%%%%%####%%%%%%%%%%%%###%%%%%%%@%%%%@@
-    ===-:-**#*+#%%%===-                    +@%#%%%#%%%%%%%#%%%%%%%%#%%%%%%%###%%%%%%%@@%@@@@+=
-    .***##%%+---                        :-#%##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@%%%@@+-  
-    :@@%+---.                             :-#%%%%%%%%%%%%%%%#%%%%%%%%%%%%%%%@@@@%%%%%@@-   
-                                                @@##%%@@@@@@@@@@@@@@@@@@@@%%%%%%@@%%%%%@@-   
-                                                @@##%%@*.*@%%%%%#%%%%%%%@@%%%%@@%%%%%%%%%%%. 
-                                            *%@@%%@*     #@#*##%%%%%%%%%@@@%%##%%%%%%%%@@: 
-                                            *@**@#         #@*#%%%%#%%%%@@*=%#*%%%@@@%%@@: 
-                                            #@+=@@           #@###%%###%%%@@**@@%%%%@+ +@@@. 
-                                            .*@#*%#             @@*#@@%####@@+=@@%%%%@+       
-                                        +@*+%@               ####@* -###@@+=@@##%%@+       
-                                        .:*%####                 ###+   =#@@+=@% .###=       
-                                        =@#+%@.                           #@++@@             
-                                    .-+###**.                           #@**@@             
-                                    =+#*+#@-                             #@**@%             
-                                =+++#*++*%@-                         :+++*#**@%             
-                                =+**###*+*%##*+.                     .+*#@@+=####+=           
-                                %@==%%@%+#@#*%@.                   .***#%@@*+@@**@#           
-                                %@%#-=@@%*-*%@@.                   :@@%*-#@@%--%%@#           
-                                ::::  :::. .:::                     :::. .:::  :::.           
-                                    """ )
-
-    if(forw == "end"): 
-        print(""" \n 
-    ███████╗██╗███╗░░██╗██╗░██████╗██╗░░██╗██╗
-    ██╔════╝██║████╗░██║██║██╔════╝██║░░██║██║
-    █████╗░░██║██╔██╗██║██║╚█████╗░███████║██║
-    ██╔══╝░░██║██║╚████║██║░╚═══██╗██╔══██║╚═╝
-    ██║░░░░░██║██║░╚███║██║██████╔╝██║░░██║██╗
-    ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝╚═════╝░╚═╝░░╚═╝╚═╝
-    """)
 
 def checkTradeDate(tradeDate_data, tradeDate, url_id, params):
 
@@ -159,30 +108,91 @@ def main(sc):
 
     logging.info("Start Process CRAW")
 
-    dataName_array = ["Currencies", "Energies" , "Equities" , "Financials" , "Grains" , "Meats" , "Metals" , "Softs"]
-    #### Standard Werte###
-    b = 0
-    progress = 0.0
-    isCme = False
-    ######################
+    wb = load_workbook('Daten.xlsx') 
 
-    BeautifulPrintouts("start")
-    
+    ws = wb.active
+
+    dataName_array = ["Currencies", "Energies" , "Equities" , "Financials" , "Grains" , "Meats" , "Metals" , "Softs"]
+
+    b = 0
+
+    progress = 0.0
+
+    isCme = False
+
+
+    cord_col_a = 1
+    cord_col_b = 2 
+
+    print(            """\n                                                                     
+                                        .......                                               
+                                :::::=@@@@@@@-::::.                                         
+                            .::-%@@%%%*******#%@@@#::::::::::::::::::.                      
+                        .---*%%%******************#%%%%%%%%####%%%%%%*-------               
+                        .=*%%%#******#%%%#*****************************#%%%%%%%===-           
+                    =+%#*********%#*#@%%#***********************************%%%#=-         
+                    ++%#***********@@+*#%@%****************####***##########*****#%#+-       
+                    @@*************%%@@%%%#************#####%%%########%%%%@#******#%#+:     
+                **@@*************##%%%%#*************#####%%%%%%###*###%%%%%*******%@=     
+                +#****%%%%@@@%#******###***********#####***##%%%##%%%###%%%%@@***###*####:   
+            +#*+====+++++++#%@@##*********+++*****##%######%###%%%%%%%%%%%%###*#%#*##@@-   
+            =#*+====*****#%%%@@@@@@####****+***###****##%%%%###%%%%%%%%%%%%%%*%#+#%#*#%##%%. 
+        =%*+==+*****@@@#+++++=--=@@@@%##%###%%%%####***####%%%%%%%%%%%%%%%%##%@%*###*#%@@: 
+        -@#===+*****@%-=+-.:---%@@@**@@%###*****%#%%%%%%%#%%%%%%%%#%%%%%%%##%%%%@@*#%#*#%@@: 
+        -@#=+***%@@@--:-+=-*@@@+=**@@%%%%%%%#░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗░░░*#%#*@@
+    :@%=+***%@+===++**@@@*==+****@@%%%%%%%%██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║░░░%%%##@@
+    :-%#++*#%*+::--=+%#++++*****@@%%%%%%%%%%██║░░╚═╝██████╔╝███████║░╚██╗████╗██╔╝░░░%%%%%@@
+    .@%=+##%+:=+=+%%%#++****#########%%%%#%%%██║░░██╗██╔══██╗██╔══██║░░████╔═████║░░░░%%%%%@@
+    --%#**%*=-:+*#%++++**###****.    .***#%%%%╚█████╔╝██║░░██║██║░░██║░░╚██╔╝░╚██╔╝░██╗@%%%%@@
+    @@=+@#===+###++****##**+:            =@%##░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝@%%%%@@
+    @@###+:=**#*+**####+-                :+*##%%%%%%%%%%%%%%%####%%%%%%%%%%%%###%%%%%%%@%%%%@@
+    ===-:-**#*+#%%%===-                    +@%#%%%#%%%%%%%#%%%%%%%%#%%%%%%%###%%%%%%%@@%@@@@+=
+    .***##%%+---                        :-#%##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@@@@%%%@@+-  
+    :@@%+---.                             :-#%%%%%%%%%%%%%%%#%%%%%%%%%%%%%%%@@@@%%%%%@@-   
+                                                @@##%%@@@@@@@@@@@@@@@@@@@@%%%%%%@@%%%%%@@-   
+                                                @@##%%@*.*@%%%%%#%%%%%%%@@%%%%@@%%%%%%%%%%%. 
+                                            *%@@%%@*     #@#*##%%%%%%%%%@@@%%##%%%%%%%%@@: 
+                                            *@**@#         #@*#%%%%#%%%%@@*=%#*%%%@@@%%@@: 
+                                            #@+=@@           #@###%%###%%%@@**@@%%%%@+ +@@@. 
+                                            .*@#*%#             @@*#@@%####@@+=@@%%%%@+       
+                                        +@*+%@               ####@* -###@@+=@@##%%@+       
+                                        .:*%####                 ###+   =#@@+=@% .###=       
+                                        =@#+%@.                           #@++@@             
+                                    .-+###**.                           #@**@@             
+                                    =+#*+#@-                             #@**@%             
+                                =+++#*++*%@-                         :+++*#**@%             
+                                =+**###*+*%##*+.                     .+*#@@+=####+=           
+                                %@==%%@%+#@#*%@.                   .***#%@@*+@@**@#           
+                                %@%#-=@@%*-*%@@.                   :@@%*-#@@%--%%@#           
+                                ::::  :::. .:::                     :::. .:::  :::.           
+                                    """ )
+
     inc = 4
-    ch = 'B' 
+    ch = 'B'
     #For Schleife -> Gehe durch Datensätze (in Data ordner)
     for b in range (0, 8):
 
         inc = 4 
+    
+        if b >= 1: 
+            cord_col_a = cord_col_a + 3
+            cord_col_b = cord_col_b + 3
 
         with open("data/" + dataName_array[b] + ".json") as f:
             info_data = json.load(f)
+
+        ws[get_column_letter(cord_col_a) + "2" ] = dataName_array[b]
+
+        cord_a = 4
+        cord_b = 5
 
         print("Fetching: " + dataName_array[b] + ".json")
 
         printProgressBar(progress)
 
         print("Progress: " + str(progress) + "%\n")
+
+        
 
         for l in range(0 , len(info_data["infoData"])):
 
@@ -242,15 +252,27 @@ def main(sc):
 
                 isCme = True
 
+                
+
+
+            ws[get_column_letter(cord_col_a) + str(cord_a)] = info_data["infoData"][l]["name"]
+            ws[get_column_letter(cord_col_a) + str(cord_b)] = "Monat:"
+            ws[get_column_letter(cord_col_b) + str(cord_b)] = "Total Volume:"
             name = info_data["infoData"][l]["name"]
             i = 0 #für schleife monate reset
             aoa = [[name], ["MONAT", "TOTAL"]]
+        
+            cord_b = cord_b + 1
 
             while i < 5:
 
                 if(isCme == True):
                     if i == len(data_Cme["monthData"]): #Exit wenn nicht mehr monate # Dann im array leer!
+                        cord_b = cord_b + 1
                         break
+    
+                    ws[get_column_letter(cord_col_a) + str(cord_b)] = data_Cme["monthData"][i]["month"]
+                    ws[get_column_letter(cord_col_b) + str(cord_b)] = data_Cme["monthData"][i]["totalVolume"]
 
                     month = data_Cme["monthData"][i]["month"]
                     totalVolume = data_Cme["monthData"][i]["totalVolume"]
@@ -259,8 +281,13 @@ def main(sc):
 
                     
                 if(isCme == False):
+
                     if i == len(data_ice): #Bug fix "Out of Range z.190"
+                        cord_b = cord_b + 1
                         break
+                    
+                    ws[get_column_letter(cord_col_a) + str(cord_b)] = data_ice[i]["marketStrip"]
+                    ws[get_column_letter(cord_col_b) + str(cord_b)] = data_ice[i]["volume"]
 
                     month = data_ice[i]["marketStrip"]
                     totalVolume = data_ice[i]["volume"]
@@ -269,6 +296,8 @@ def main(sc):
 
 
                 i = i + 1 #Increment
+            
+                cord_b = cord_b + 1
 
             inc = inc + 8
 
@@ -279,12 +308,23 @@ def main(sc):
             except: 
                 logging.exception("Connection error - could not be pushed to sheet")
             
+            #Abstand zwischen neuen Datensätzen:
+            cord_b = cord_b + 2
+            cord_a = cord_a + 6
+
             i = 0 #reset für nächsten durchlauf
 
         ch = chr(ord(ch) + 3) #Increment in GoogleSheet(-> Jeder Datensatz soll nebeneinander stehen B wird zu B+3=E)
+        wb.save("Daten.xlsx")
 
-    BeautifulPrintouts("end")
-
+    print(""" \n 
+    ███████╗██╗███╗░░██╗██╗░██████╗██╗░░██╗██╗
+    ██╔════╝██║████╗░██║██║██╔════╝██║░░██║██║
+    █████╗░░██║██╔██╗██║██║╚█████╗░███████║██║
+    ██╔══╝░░██║██║╚████║██║░╚═══██╗██╔══██║╚═╝
+    ██║░░░░░██║██║░╚███║██║██████╔╝██║░░██║██╗
+    ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝╚═════╝░╚═╝░░╚═╝╚═╝
+    """)
     logging.info("Finished Process CRAW")
     s.enter(3600, 1, main, (sc,)) #Run every hour
     
